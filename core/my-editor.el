ebfcb17e8b88851601fb3e9b2a4de51f0a9e90a5
Amin Ogarrio <amin.ogarrio@gmail.com>
2015-10-13 10:56:30 -0700

    Ignore mouse scroll.

--- core/my-editor.el
+++ core/my-editor.el
@@ -14,6 +14,22 @@
 (setq initial-major-mode 'org-mode)
 (setq echo-keystrokes 0.1)
 
+;; Disaling mouse scrilling
+(mouse-wheel-mode -1)
+
+(global-set-key [wheel-up] 'ignore)
+(global-set-key [wheel-down] 'ignore)
+(global-set-key [wheel-left] 'ignore)
+(global-set-key [wheel-right] 'ignore)
+(global-set-key [double-wheel-up] 'ignore)
+(global-set-key [double-wheel-down] 'ignore)
+(global-set-key [double-wheel-left] 'ignore)
+(global-set-key [double-wheel-right] 'ignore)
+(global-set-key [triple-wheel-up] 'ignore)
+(global-set-key [triple-wheel-down] 'ignore)
+(global-set-key [triple-wheel-left] 'ignore)
+(global-set-key [triple-wheel-right] 'ignore)
+
 ;; store all backup and autosave files in the tmp dir
 (setq backup-directory-alist `(("." . ,backup-dir)))
 (setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
