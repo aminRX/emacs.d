681d8bd709b912f6f31a1a5764777d6b5853745c
Amin Ogarrio <amin.ogarrio@gmail.com>
2016-03-20 22:12:28 -0700

    web-mode without eslint.

--- core/my-editor.el
+++ core/my-editor.el
@@ -178,7 +178,6 @@
 ;; disable jshint since we prefer eslint checking
 (setq-default flycheck-disabled-checkers (append flycheck-disabled-checkers '(javascript-jshint)))
 ;; use eslint with web-mode for jsx files
-(flycheck-add-mode 'javascript-eslint 'web-mode)
 (flycheck-add-mode 'javascript-eslint 'js2-mode)
 
 (require 'nlinum)
