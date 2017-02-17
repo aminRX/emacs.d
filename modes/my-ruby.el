518f6577b67329725890746a478d3e8f9da36853
Amin Ogarrio <amin.ogarrio@gmail.com>
2017-02-17 00:39:42 -0700

    Add ruby hook with lambdas.

--- modes/my-ruby.el
+++ modes/my-ruby.el
@@ -29,6 +29,11 @@
 ;; Ruby projects
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
 
+(add-hook 'ruby-mode-hook (lambda ()
+                            (inf-ruby-minor-mode +1)
+                            (ruby-tools-mode +1)
+                            ;; CamelCase aware editing operations
+                            (subword-mode +1)))
 
 (provide 'my-ruby)
 
